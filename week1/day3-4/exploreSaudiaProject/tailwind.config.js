1b4977fb6e1a5351b821169521b6818b05356571
MashhoodYousafGhuman <mashhoodyousaf24@gmail.com>
2025-07-31 06:53:38 -0700

    exploreSaudiaProject

---
+++ week1/day3-4/exploreSaudiaProject/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html", // This will include all HTML files in your project
+    "./**/*.{html,js}", // If you have files in src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
