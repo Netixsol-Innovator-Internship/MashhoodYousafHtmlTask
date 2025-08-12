0b569a982f8374b9619cb0416c1813a915468797
MashhoodYousafGhuman <mashhoodyousaf24@gmail.com>
2025-08-12 13:14:45 +0500

    add vercel file for swagger

--- week3/day1-BackEnd/app.js
+++ week3/day1-BackEnd/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
-const { toASCII } = require("punycode");
 
 const app = express();
 
