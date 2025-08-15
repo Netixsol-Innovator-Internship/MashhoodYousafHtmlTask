f048d3e07b3c9346d1f43370dfddc05965bba2a8
MashhoodYousafGhuman <mashhoodyousaf24@gmail.com>
2025-08-16 01:47:22 +0500

    now only admin can perfrom CRUD

--- week3/hackthon-w3/ecommerceBackEnd/src/middlewares/verifyToken.js
+++ week3/hackthon-w3/ecommerceBackEnd/src/middlewares/verifyToken.js
@@ -1,31 +1,53 @@
 const jwt = require("jsonwebtoken");
+const ErrorResponse = require("../utils/errorResponse");
 
 const JWT_KEY = "chooseAnyStrongKey";
 
-module.exports = (req, res, next) => {
+const checkAuth = (req, res, next) => {
   if (req.method === "OPTIONS") {
     return next();
   }
   try {
     if (!req.headers.authorization) {
-      throw new Error("No authorization header");
+      const error = new ErrorResponse(
+        "No authorization header",
+        404,
+        {},
+        false
+      );
+      return next(error);
     }
 
     const token = req.headers.authorization.split(" ")[1];
     if (!token) {
-      throw new Error("Authentications Failed");
+      const error = new ErrorResponse("No Token,Auth Failed", 404, {}, false);
+      return next(error);
     }
     const decodedToken = jwt.verify(token, JWT_KEY);
-    req.userData = { userId: decodedToken.userId };
+    req.userData = { userId: decodedToken.userId, role: decodedToken.role };
     console.log("token", token);
     next();
   } catch (err) {
     console.log("err", err);
-    const error = new Error(
-      "Authentications Failed, token...",
-    );
-    error.status = 401;
-    console.log("error", error);
+    const error = new Error("Authentications Failed, token...", 500, {}, false);
     return next(error);
   }
+};
+
+const checkAdmin = (req, res, next) => {
+  if (req.userData.role !== "admin") {
+    const error = new ErrorResponse(
+      "Only Admin can access this",
+      403,
+      {},
+      false
+    );
+    return next(error);
+  }
+  next();
+};
+
+module.exports = {
+  checkAuth,
+  checkAdmin,
 };
--- week3/hackthon-w3/ecommerceBackEnd/src/routes/productRoutes.js
+++ week3/hackthon-w3/ecommerceBackEnd/src/routes/productRoutes.js
@@ -2,17 +2,23 @@ const express = require("express");
 
 const productController = require("../controllers/productController");
 const upload = require("../middlewares/multerCloudinary");
-const checkToken = require("../middlewares/verifyToken");
+const { checkAuth, checkAdmin } = require("../middlewares/verifyToken");
 
 const router = express.Router();
 
-router.get("/stats", productController.getStats);
+// router.get("/stats", productController.getStats);
 router.get("/", productController.getProducts);
 
 // protectedRoutes
-router.post("/", upload.single("image"), productController.createProduct);
-router.get("/:id",  productController.getProductsByID);
-router.put("/:id",  productController.updateProducts);
-router.delete("/:id",  productController.deleteProduct);
+router.post(
+  "/",
+  checkAuth,
+  checkAdmin,
+  upload.single("image"),
+  productController.createProduct
+);
+router.get("/:id", productController.getProductsByID);
+router.put("/:id", checkAuth, checkAdmin, productController.updateProducts);
+router.delete("/:id", checkAuth, checkAdmin, productController.deleteProduct);
 
 module.exports = router;
